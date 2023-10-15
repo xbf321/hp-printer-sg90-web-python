22519b56de3ddf4eec563770102ebcded47da5c5
xingbaifang <xingbaifang@meituan.com>
2023-10-15 20:59:48 +0800

    Update sg90s.py

--- sg90s.py
+++ sg90s.py
@@ -2,23 +2,20 @@
 #-*- coding: utf-8 -*- 
 # 30 11 * * * /hp-printer-sg90-web-python/sg90s.py
 import configparser
-config = configparser.ConfigParser()
-config.read('config.ini', encoding='utf-8')
-pin = config['DEFAULT']['pin']
-end_angle = config['DEFAULT']['end_angle']
-# items = con.items('url')
-
-print(pin)
-print(end_angle)
-"""
 import RPi.GPIO as GPIO
 import time
 import signal
 import atexit
-  
-atexit.register(GPIO.cleanup)
 
-pin = 32
+# atexit.register(GPIO.cleanup)
+
+config = configparser.ConfigParser()
+config.read('config.ini', encoding='utf-8')
+pin = int(config['DEFAULT'].get('pin', 32))
+end_angle = int(config['DEFAULT'].get('end_angle', 120))
+
+#pin = 32
+#end_angle = 116
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.OUT)
@@ -34,8 +31,23 @@ def moveToAngle(angle):
   time.sleep(0.02)
   # 归零信号
   pwm.ChangeDutyCycle(0)           
-  time.sleep(0.2)
+  time.sleep(0.02)
 
+
+moveToAngle(0)
+time.sleep(1)
+
+for i in range(0,end_angle,3): 
+    moveToAngle(i)
+
+time.sleep(3)
+
+for i in range(end_angle,0,-3):  
+    moveToAngle(i)
+
+pwm.stop()
+GPIO.cleanup
+"""
 while(True):
   for i in range(0,181,10): 
     moveToAngle(i)
